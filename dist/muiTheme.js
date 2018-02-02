e838a069c656c68bf01770143db9065bf0e164cb
Jack McCown <jmccown@cbinsights.com>
2018-02-02 03:15:56 -0500

    trying again

--- dist/muiTheme.js
+++ dist/muiTheme.js
@@ -3,9 +3,6 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 exports.muiTheme = muiTheme;
 
 var _react = require('react');
@@ -26,15 +23,73 @@ var _ = require('./');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var lightBaseTheme = (0, _styles.createMuiTheme)();
-var darkBaseTheme = (0, _styles.createMuiTheme)({
-    palette: (0, _palette2.default)({
-        type: 'dark'
-    })
-});
+var accent = {
+    50: '#ffede7',
+    100: '#ffd1c2',
+    200: '#ffb399',
+    300: '#ff9470',
+    400: '#ff7d52',
+    500: '#ff6633',
+    600: '#ff5e2e',
+    700: '#ff5327',
+    800: '#ff4920',
+    900: '#ff3814',
+    A100: '#ffffff',
+    A200: '#fffbfb',
+    A400: '#ffcec8',
+    A700: '#ffb8ae',
+    contrastDefaultColor: 'light'
+    // contrastDarkColors: [
+    //   '50',
+    //   '100',
+    //   '200',
+    //   '300',
+    //   '400',
+    //   '500',
+    //   '600',
+    //   '700',
+    //   'A100',
+    //   'A200',
+    //   'A400',
+    //   'A700'
+    // ],
+    // contrastLightColors: ['800', '900']
+};
+var primary = {
+    50: '#e0edf3',
+    100: '#b3d1e0',
+    200: '#80b3cc',
+    300: '#4d94b8',
+    400: '#267da8',
+    500: '#006699',
+    600: '#005e91',
+    700: '#005386',
+    800: '#00497c',
+    900: '#00386b',
+    A100: '#9ac6ff',
+    A200: '#67aaff',
+    A400: '#348dff',
+    A700: '#1a7fff',
+    contrastDefaultColor: 'light'
+    // contrastDarkColors: ['50', '100', '200', '300', 'A100', 'A200'],
+    // contrastLightColors: [
+    //   '400',
+    //   '500',
+    //   '600',
+    //   '700',
+    //   '800',
+    //   '900',
+    //   'A400',
+    //   'A700'
+    // ]
+};
 
-lightBaseTheme.themeName = 'Light Theme';
-darkBaseTheme.themeName = 'Dark Theme';
+var currentTheme = (0, _styles.createMuiTheme)({
+    palette: {
+        primary: primary,
+        accent: accent
+    }
+});
 
 var previewStyle = function previewStyle(color) {
     return {
@@ -47,13 +102,6 @@ var previewStyle = function previewStyle(color) {
 
 function muiTheme() {
     var theme = arguments.length <= 0 ? undefined : arguments[0];
-    var currentTheme = lightBaseTheme;
-    if (theme === 'dark') {
-        currentTheme = darkBaseTheme;
-    }
-    if ((typeof theme === 'undefined' ? 'undefined' : _typeof(theme)) === 'object') {
-        currentTheme = theme;
-    }
 
     var backgroundColor = currentTheme.palette.type === 'dark' ? 'rgb(50,50,50)' : 'white';
     return function (storyFn, context) {
